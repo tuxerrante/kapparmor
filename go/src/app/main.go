a281d89d97a1fbae76b73cb3e3729fa01ec1ed8f
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2022-11-21 12:17:25 +0100

    check profiler and files access

--- go/src/app/main.go
+++ go/src/app/main.go
@@ -7,32 +7,65 @@ import (
 	"strconv"
 )
 
+var (
+	DIRNAME       string = os.Getenv("PROFILES_DIR")
+	POLL_TIME_ARG string = os.Getenv("POLL_TIME")
+)
+
+const (
+	profiler = "/sbin/apparmor_parser"
+)
+
 func main() {
-	// TODO: add default values if the env var is missing
-	var DIRNAME string  = os.Getenv("PROFILES_DIR")
-	// TODO: check value before converting to int
-	POLL_TIME, err := strconv.Atoi(os.Getenv("POLL_TIME"))
+
+	// Type check
+	POLL_TIME, err := strconv.Atoi(POLL_TIME_ARG)
 	if err != nil {
-		log.Fatalf("Was not possible to convert env var POLL_TIME %v to an integer", POLL_TIME)
+		log.Fatalf("It was not possible to convert env var POLL_TIME %v to an integer.\n%v", POLL_TIME, err)
 	}
 
-	log.Printf("Polling directory %s every %d seconds.", DIRNAME, POLL_TIME)
+	fmt.Printf("> Polling directory %s every %d seconds.\n", DIRNAME, POLL_TIME)
+
+	// Check profiler binary
+	// TODO: check also for permissions
+	if _, err := os.Stat(profiler); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	// Check profiles are readable
+	if !areProfilesReadable() {
+		log.Fatalf("There was an error accessing the files in %s.\n", DIRNAME)
+	}
+
+	// Poll configmap forever every POLL_TIME seconds
+	pollProfiles(POLL_TIME)
+}
+
+func areProfilesReadable() bool {
 
 	files, err := os.ReadDir(DIRNAME)
-	if err != nil  {
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	// TODO: Should the app terminate if no profiles are present?
+	if len(files) == 0 {
+		fmt.Printf("No files were found in the given folder!\n")
+		return true
+	}
+
+	fmt.Printf("Found files in given folder:\n")
 	for _, file := range files {
 		if file.IsDir() {
 			fmt.Printf("Directory '%s' will be skipped.\n", file.Name())
 			continue
 		}
-		fmt.Printf("- %s\n", file.Name())		
+		fmt.Printf("- %s\n", file.Name())
 	}
 
-	// TODO: filter only the files recently changed
-	// 	Is not safe to count only the current time - POLL_TIME since the last scan could have been failed.
+	return true
+}
 
-	// TODO: move or directly apply profiles
+func pollProfiles(delay int) {
+	fmt.Printf("> pollProfiles TBD %d\n", delay)
 }
