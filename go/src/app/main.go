26a4eca192d519ef46e9d1b8da871dc63ccbbeee
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2022-11-22 16:26:33 +0100

    compare profiles by checksum.

--- go/src/app/main.go
+++ go/src/app/main.go
@@ -1,19 +1,31 @@
 package main
 
+//
+// TODO:
+//  - unload profiles present only in loadedProfiles and not in NewProfiles
+//  - manage symlinks: on the node there could be already some custom profile
+//
 import (
+	"bufio"
+	"bytes"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var (
-	DIRNAME       string = os.Getenv("PROFILES_DIR")
-	POLL_TIME_ARG string = os.Getenv("POLL_TIME")
+	CONFIGMAP_PATH string = os.Getenv("PROFILES_DIR")
+	POLL_TIME_ARG  string = os.Getenv("POLL_TIME")
 )
 
 const (
-	profiler = "/sbin/apparmor_parser"
+	KERNEL_PATH  = "/sys/kernel/security/apparmor/profiles"
+	PROFILER_BIN = "/sbin/apparmor_parser"
 )
 
 func main() {
@@ -21,29 +33,30 @@ func main() {
 	// Type check
 	POLL_TIME, err := strconv.Atoi(POLL_TIME_ARG)
 	if err != nil {
-		log.Fatalf("It was not possible to convert env var POLL_TIME %v to an integer.\n%v", POLL_TIME, err)
+		log.Fatalf(">> It was not possible to convert env var POLL_TIME %v to an integer.\n%v", POLL_TIME, err)
 	}
 
-	fmt.Printf("> Polling directory %s every %d seconds.\n", DIRNAME, POLL_TIME)
+	fmt.Printf("> Polling directory %s every %d seconds.\n", CONFIGMAP_PATH, POLL_TIME)
 
 	// Check profiler binary
-	// TODO: check also for permissions
-	if _, err := os.Stat(profiler); os.IsNotExist(err) {
+	if _, err := os.Stat(PROFILER_BIN); os.IsNotExist(err) {
 		log.Fatal(err)
 	}
 
-	// Check profiles are readable
-	if !areProfilesReadable() {
-		log.Fatalf("There was an error accessing the files in %s.\n", DIRNAME)
+	// Check profile directory accessibility
+	profilesAreReadable, _ := areProfilesReadable(CONFIGMAP_PATH)
+	if !profilesAreReadable {
+		log.Fatalf(">> There was an error accessing the files in %s.\n", CONFIGMAP_PATH)
 	}
 
 	// Poll configmap forever every POLL_TIME seconds
 	pollProfiles(POLL_TIME)
 }
 
-func areProfilesReadable() bool {
+func areProfilesReadable(FOLDER_NAME string) (bool, []string) {
 
-	files, err := os.ReadDir(DIRNAME)
+	filenames := []string{}
+	files, err := os.ReadDir(CONFIGMAP_PATH)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -51,7 +64,7 @@ func areProfilesReadable() bool {
 	// TODO: Should the app terminate if no profiles are present?
 	if len(files) == 0 {
 		fmt.Printf("No files were found in the given folder!\n")
-		return true
+		return true, nil
 	}
 
 	fmt.Printf("Found files in given folder:\n")
@@ -61,11 +74,115 @@ func areProfilesReadable() bool {
 			continue
 		}
 		fmt.Printf("- %s\n", file.Name())
+		filenames = append(filenames, file.Name())
 	}
 
-	return true
+	return true, filenames
 }
 
+// Profiles will probably change content while keeping the same name, so a digest comparison
+// can be very useful if we don't want to load everything every time.
+// https://pkg.go.dev/github.com/opencontainers/go-digest#section-readme
 func pollProfiles(delay int) {
-	fmt.Printf("> pollProfiles TBD %d\n", delay)
+
+	ticker := time.NewTicker(time.Duration(delay) * time.Second)
+	for range ticker.C {
+		newProfiles, err := loadNewProfiles()
+		if err != nil {
+			log.Fatalf(">> Error retrieving profiles: %v\n%v", newProfiles, err)
+		}
+
+		// TODO: unload removed profiles
+	}
+}
+
+// Check if the current profiles are really new
+func loadNewProfiles() ([]string, error) {
+	loadedProfiles, err := getLoadedProfiles()
+	if err != nil {
+		log.Fatalf(">> Error reading existing profiles.\n%v", err)
+	}
+
+	for _, profile := range loadedProfiles {
+		fmt.Println(profile)
+	}
+
+	newProfiles, err := getNewProfiles()
+	if err != nil {
+		log.Fatalf(">> Error reading new profiles in the configmap!\n%v", err)
+	}
+
+	filteredNewProfiles := make([]string, len(newProfiles))
+	for k := range newProfiles {
+		if loadedProfiles[k] == nil || !bytes.Equal(newProfiles[k], loadedProfiles[k]) {
+			filteredNewProfiles = append(filteredNewProfiles, k)
+		}
+	}
+
+	obsoleteProfiles := make([]string, len(loadedProfiles))
+	for k := range loadedProfiles {
+		if newProfiles[k] == nil {
+			obsoleteProfiles = append(obsoleteProfiles, k)
+		}
+	}
+
+	// Execute apparmor_parser --replace --verbose filteredNewProfiles
+	fmt.Println("> TODO: apparmor_parser --replace ", filteredNewProfiles)
+
+	// Execute apparmor_parser -R obsoleteProfiles
+	fmt.Println("> TODO: apparmor_parser -R ", obsoleteProfiles)
+
+	return filteredNewProfiles, nil
+}
+
+func getNewProfiles() (map[string][]byte, error) {
+	return getProfiles(CONFIGMAP_PATH)
+}
+func getLoadedProfiles() (map[string][]byte, error) {
+	return getProfiles(KERNEL_PATH)
+}
+
+// Search for profiles already present on the current node in '$apparmorfs/profiles' folder
+// - to be more efficient I could create a profiles map with a capacity as big as the number of files in the folder
+func getProfiles(profilesPath string) (map[string][]byte, error) {
+
+	profilesFile, err := os.Open(profilesPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open %s: %v", profilesPath, err)
+	}
+	defer profilesFile.Close()
+
+	profiles := map[string][]byte{}
+	currentHash := sha256.New()
+	scanner := bufio.NewScanner(profilesFile)
+
+	for scanner.Scan() {
+		profileName := parseProfileName(scanner.Text())
+		if profileName == "" {
+			// Unknown line format; skip it.
+			continue
+		}
+		// TODO: save the digest of the profile in the new map
+		if _, err := io.Copy(currentHash, profilesFile); err != nil {
+			log.Fatal(err)
+		}
+		// profiles[profileName] = true
+		profiles[profileName] = currentHash.Sum(nil)
+	}
+	return profiles, nil
+}
+
+// The profiles file is formatted with one profile per line, matching a form:
+//
+//	namespace://profile-name (mode)
+//	profile-name (mode)
+//
+// Where mode is {enforce, complain, kill}. The "namespace://" is only included for namespaced
+// profiles. For the purposes of Kubernetes, we consider the namespace part of the profile name.
+func parseProfileName(profileLine string) string {
+	modeIndex := strings.IndexRune(profileLine, '(')
+	if modeIndex < 0 {
+		return ""
+	}
+	return strings.TrimSpace(profileLine[:modeIndex])
 }
