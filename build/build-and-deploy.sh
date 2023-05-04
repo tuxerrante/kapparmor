fbfbe26f6f6ecf6d40e1bb569a91c1457edd4ab3
Alessandro Affinito <affinito.ale@gmail.com>
2023-05-04 13:03:10 +0200

    local build and deploy CI

---
+++ build/build-and-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+. ./build/build-app.sh
+. ./config/secrets
+
+echo $GHCR_TOKEN | docker login -u "$(git config user.email)" --password-stdin ghcr.io
+docker push ghcr.io/tuxerrante/kapparmor:${APP_VERSION}_dev
+
+# Install the chart from the local directory
+    helm upgrade kapparmor --install \
+        --atomic \
+        --timeout 60s \
+        --debug \
+        --set image.tag=${APP_VERSION}_dev \
+        --set image.pullPolicy=Always \
+        --dry-run \
+        charts/kapparmor
+echo
+echo "> Is the previous result the expected one?"
+echo "> Current K8S context:" "$(kubectl config current-context)"
+read -r -p "> Are you sure? [Y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    helm upgrade kapparmor --install \
+        --atomic \
+        --timeout 60s \
+        --debug \
+        --set image.tag=${APP_VERSION}_dev \
+        --set image.pullPolicy=Always \
+        charts/kapparmor
+else
+    echo " Bye."
+    echo
+fi
---
+++ build/build-app.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+source ./config/config
+
+# Clean old images
+echo "> Removing old and dangling old images..."
+docker rmi "$(docker images --filter "reference=ghcr.io/tuxerrante/kapparmor" -q --no-trunc )"
+
+# go build -o ./.go/bin ./...
+# go test -v -coverprofile=coverage.out -covermode=atomic ./go/src/app/...
+if [[ ! -f ".go/bin/golangci-lint" ]]; then
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ./.go/bin
+fi
+echo "> Linting..."
+./.go/bin/golangci-lint run
+
+echo "> Scanning for suspicious constructs..."
+go vet go/...
+
+echo "> Creating test output..."
+docker build --target test-coverage --tag "ghcr.io/tuxerrante/kapparmor:${APP_VERSION}_dev" .
+
+#### To run it look into docs/testing.md
+echo "> Building container image..."
+docker build --tag "ghcr.io/tuxerrante/kapparmor:${APP_VERSION}_dev" \
+    --no-cache	  \
+    --build-arg POLL_TIME=60 --build-arg PROFILES_DIR=/app/profiles   \
+    -f Dockerfile \
+    .
+
+# docker run --rm -it --privileged --mount type=bind,source='/sys/kernel/security',target='/sys/kernel/security' --mount type=bind,source='/etc',target='/etc' --name kapparmor  ghcr.io/tuxerrante/kapparmor:${APP_VERSION}_dev
