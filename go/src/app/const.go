638f68d74d1a350a5ef3345008f5f3075560baf0
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2025-10-29 18:20:27 +0100

    go 1.25
    Signed-off-by: Alessandro Affinito <alessandro.affinito@ptvgroup.com>

---
+++ go/src/app/const.go
@@ -0,0 +1,8 @@
+// Package main contains constant definitions used across the application.
+package main
+
+const (
+	MaxAllowedPollingTime = 86400 // 24 hours
+	ProfilerBin           = "apparmor_parser"
+	ProfileNamePrefix     = "custom."
+)
