49c146e9a0146d595965a1fe9ae82327911c0055
Alessandro Affinito <affinito.ale@gmail.com>
2025-11-07 10:01:33 +0100

    moved src folder
    Signed-off-by: Alessandro Affinito <affinito.ale@gmail.com>

--- go/src/app/const.go
+++
@@ -1,8 +0,0 @@
-// Package main contains constant definitions used across the application.
-package main
-
-const (
-	MaxAllowedPollingTime = 86400 // 24 hours
-	ProfilerBin           = "apparmor_parser"
-	ProfileNamePrefix     = "custom."
-)
