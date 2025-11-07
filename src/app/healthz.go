9e8bb4335a2f6d6cd8d72b4eae7679ba2c5b54a0
Alessandro Affinito <affinito.ale@gmail.com>
2025-11-07 10:02:06 +0100

    Metrics server for /healthz and /readyz
    Signed-off-by: Alessandro Affinito <affinito.ale@gmail.com>

---
+++ src/app/healthz.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
+func startHealthzServer(cfg *AppConfig) {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		_, desired := getNewProfiles(cfg)
+		_, loaded, _ := getLoadedProfiles(cfg)
+
+		inSync := true
+
+		if len(desired) == len(loaded) {
+			for profile := range desired {
+				if !loaded[profile] {
+					inSync = false
+
+					break
+				}
+			}
+
+			if inSync {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("READY"))
+			} else {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				w.Write([]byte("NOT_READY"))
+			}
+		}
+	})
+
+	go func() {
+		slog.Default().Info("Starting healthz server",
+			slog.Int("port", HealthzPort),
+			slog.String("health_endpoint", "/healthz"),
+			slog.String("ready_endpoint", "/readyz"))
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", HealthzPort), nil); err != nil {
+			slog.Default().Error("Healthz server failed", slog.Any("error", err))
+		}
+	}()
+}
